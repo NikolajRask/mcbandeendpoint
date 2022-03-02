97268e171de64185b61b65d49e1e5809109a999d
NikolajRask <83177244+NikolajRask@users.noreply.github.com>
2022-03-02 19:10:12 +0100

    Updating JSON parse

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send({message: "Hello World"}));
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
