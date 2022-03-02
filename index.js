13aaba4bf4d6a392a703660c9335c702604decbb
NikolajRask <83177244+NikolajRask@users.noreply.github.com>
2022-03-02 23:42:32 +0100

    Opdater bande post og get

--- index.js
+++ index.js
@@ -2,6 +2,48 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send({message: "Hello World"}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+var bander = {}
+var APIKEY = "ECJneBBEnjeNCEYHbsmuEC"
+
+app.get('/', (req, res) => res.send({message: "Kom ind og spil"}));
+
+app.post('/bande', (req,res) => {
+  if (req.body.key) {
+    if (req.body.key == APIKEY) {
+        if (!req.body.bande) {res.status(400).send("Bande navn mangler")}
+        if (!req.body.ejer) {res.status(400).send("Bande ejer mangler")}
+        if (!req.body.level) {res.status(400).send("Bande level mangler")}
+        bander[req.body.bande] = {bande: req.body.bande,ejer: req.body.ejer, level: req.body.level}
+        console.log('Bande created')
+        res.status(201).send("Bande oprettet")
+    } else {
+        res.status(401).send({status: 401, message: "Authentacation failed"})
+    }
+  } else {
+    res.status(401).send({status: 401, message: "Authentacation failed"})
+}
+})
+
+app.get('/bande/:name', (req,res) => {
+    if (req.body.key) {
+        if (req.body.key == APIKEY) {
+            const { name } = req.params
+            if (bander[name]) {
+                res.status(200).send(bander[name])
+            } else {
+                res.status(404).send({status: 404, message: "Banden eksisterer ikke"})
+            }
+        } else {
+            res.status(401).send({status: 401, message: "Authentacation failed"})
+        }
+    } else {
+        res.status(401).send({status: 401, message: "Authentacation failed"})
+    }
+
+})
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
+
